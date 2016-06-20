5607909d31c24a4507603a704feaf4573c993453
Lee Seongjoo <seongjoo@codebasic.co>
2016-06-20 15:59:47 +0900

    패키지 설치 스크립트 추가

---
+++ packages/bigpy/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bigpy',
+    version='0.1',
+    description='Make things easier for data analyis and automation',
+    packages=['webscrap'],
+    install_requires=[
+        'imapclient'
+    ])
