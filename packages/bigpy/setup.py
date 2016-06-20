df54e7f4540f54303324a02f9d17a8a7958590c0
Lee Seongjoo <seongjoo@codebasic.co>
2016-06-20 16:21:27 +0900

    설정 변경

--- packages/bigpy/setup.py
+++ packages/bigpy/setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(name='bigpy',
     version='0.1',
     description='Make things easier for data analyis and automation',
+    author='Lee Seongjoo',
+    author_email='seongjoo@codebasic.co',
+    py_modules=['mail'],
     packages=['webscrap'],
     install_requires=[
         'imapclient'
